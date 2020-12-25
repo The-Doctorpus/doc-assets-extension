ef88341f7e8d8a86d8cce55ea94d8922b53d7d53
The-Doctorpus <45578287+The-Doctorpus@users.noreply.github.com>
2020-12-24 18:39:02 -0600

    updated to fix a bug
    gray elephant seal was northern elephant seal as a placeholder, forgot to update.

--- background.js
+++ background.js
@@ -217,7 +217,7 @@ var bluelobster = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/ma
 var voltaeel = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Volta%20Electric%20Eel%20(Docassets).png"
 var redeel = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Red%20Electric%20Eel%20(Docassets).png"
 var northernelephantseal = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Northern%20Elephant%20Seal%20(Docassets).png"
-var grayelephantseal = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Northern%20Elephant%20Seal%20(Docassets).png"
+var grayelephantseal = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Gray%20Elephant%20Seal%20(Docassets).png"
 var sandbarshark = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Sandbar%20Shark%20(Docassets).png"
 
 var southerncavefish = "https://raw.githubusercontent.com/The-Doctorpus/doc-assets/main/images/skins/Southern%20Cavefish%20(Docassets).png"
